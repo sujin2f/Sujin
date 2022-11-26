5960e5e43c9d4e84d966e68ad721687ac78d3fa9
Sujin Choi <sujin.2f@gmail.com>
2022-11-26 16:58:05 -0500

    Prod error

--- src/frontend/components/RecentPosts.tsx
+++ src/frontend/components/RecentPosts.tsx
@@ -3,26 +3,24 @@ import React, { Fragment } from 'react'
 
 import { ListItem } from 'src/frontend/components/ListItem'
 import { Post } from 'src/types/wordpress'
-import { useArchive } from '../hooks/useArchive'
+import { useRecentPosts } from '../hooks/useRecentPosts'
 
 export const RecentPosts = (): JSX.Element => {
-    const { archive, loading, error } = useArchive('recent_posts', '', 0)
+    const { recentPost } = useRecentPosts()
 
-    if (loading || error) {
+    if (!recentPost.length) {
         return <Fragment />
     }
 
     return (
-        <section className="widget recent-posts show-for-large-only">
-            {archive!.posts &&
-                archive!.posts &&
-                archive!.posts.map((item: Post) => (
-                    <ListItem
-                        key={`recent-post-id-${item.slug}`}
-                        item={item}
-                        thumbnailKey={{ desktop: 'small', mobile: 'small' }}
-                    />
-                ))}
+        <section className="widget recent-posts show-for-large">
+            {recentPost.map((item: Post) => (
+                <ListItem
+                    key={`recent-post-id-${item.slug}`}
+                    item={item}
+                    thumbnailKey={{ desktop: 'small', mobile: 'small' }}
+                />
+            ))}
         </section>
     )
 }
--- src/frontend/hooks/useRecentPosts.ts
+++ src/frontend/hooks/useRecentPosts.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client'
 import { RecentReturnType, GraphQuery } from 'src/constants/graphql'
 
 export const useRecentPosts = () => {
-    const { data } = useQuery<RecentReturnType>(GraphQuery.ARCHIVE)
+    const { data } = useQuery<RecentReturnType>(GraphQuery.RECENT)
     const recentPost = (data && data.recent) || []
 
     return { recentPost }
