484f572dc91270e449769ba4c69b1ab626806c60
Sujin Choi <sujin.2f@gmail.com>
2025-04-25 13:04:02 -0400

    git fix

--- common/data/mongo/connection.ts
+++ common/data/mongo/connection.ts
@@ -8,9 +8,10 @@ if (!process.env.MONGO) {
 
 const uri = process.env.MONGO
 const options = { appName: 'devrel.template.nextjs' }
+const port = process.env.MONGO_PORT || '27017'
 const connection = IS_TEST
-    ? `mongodb://${uri}:27018`
-    : `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${uri}:27017/${process.env.MONGO_DATABASE}?authSource=${process.env.MONGO_DATABASE}`
+    ? `mongodb://${uri}:${port}`
+    : `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${uri}:${port}/${process.env.MONGO_DATABASE}?authSource=${process.env.MONGO_DATABASE}`
 
 const getClient = async () => await MongoClient.connect(connection, options)
 console.log(connection)
