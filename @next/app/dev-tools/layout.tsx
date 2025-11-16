7f489d2fcd2ca20eda1fe60e0c08e201669a6877
Sujin Choi <sujin.2f@gmail.com>
2025-11-15 21:11:45 -0500

    devtool, design system

---
+++ @next/app/dev-tools/layout.tsx
@@ -0,0 +1,26 @@
+import type { PropsWithChildren } from 'react'
+import type { Metadata } from 'next/types'
+/* Components */
+import Wrapper from '@lib/components/Wrapper'
+/* Utils */
+import { getMetaData } from '@lib/utils/server'
+/* CONSTANTS */
+import { MENU_NAMES } from '@sujin/lib/constants'
+/* Assets */
+import './layout.scss'
+
+export const generateMetadata = async (): Promise<Metadata> => {
+    const metadata = await getMetaData()
+
+    return {
+        ...metadata,
+        title: `Sujin | Dev Tool | ${metadata.title}`,
+        openGraph: {
+            title: `Sujin | ${metadata}`,
+        },
+    }
+}
+
+export default async function Layout({ children }: PropsWithChildren) {
+    return <Wrapper menu={MENU_NAMES.DEV_TOOL}>{children}</Wrapper>
+}
