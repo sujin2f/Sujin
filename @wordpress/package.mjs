837413aaf70eb5d4703ba41c32405cb384d5bddf
Sujin Choi <sujin.2f@gmail.com>
2025-11-24 21:36:03 -0500

    Production: wordpress

---
+++ @wordpress/package.mjs
@@ -0,0 +1,93 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import { exec } from 'node:child_process'
+import { config } from 'dotenv'
+import util from 'util'
+config()
+const execPromise = util.promisify(exec);
+
+const dirTemp = path.join('temp')
+const files = {
+    envDev: '.env',
+    envProd: '.env.production',
+}
+
+// Version
+import packageJson from './package.json' with { type: 'json' }
+const VERSION = packageJson.version
+
+// Overwrite VERSION info
+let env = await fs.promises.readFile(path.join(files.envProd), 'utf-8')
+env = env.replace(/VERSION=[0-9.beta-]+\n\r/, '')
+env += `VERSION=${VERSION}\n\r`
+await fs.promises.writeFile(path.join(files.envDev), env)
+console.log('🤟 \x1B[32m- Version updated. \x1B[0m')
+
+// Check if Docker image exists
+const image = `sujin2f/wordpress:${VERSION}`
+const { stdout, stderr } = await execPromise(`docker image ls ${image}`)
+if (stdout.includes(image)) {
+    console.error(`⛈️ Image ${image} already exists.`)
+    process.exit(1)
+}
+if (stderr) {
+    console.error(`👀 stderr: ${stderr}`)
+    process.exit(1)
+}
+
+const createDirectories = async () => {
+    console.log('🤟 \x1B[32m- Creating directories... \x1B[0m')
+    await fs.promises.mkdir(dirTemp)
+}
+
+const backupFiles = async () => {
+    console.log('🤟 \x1B[32m- Backup files... \x1B[0m')
+    // .env
+    await fs.promises.copyFile(path.join(files.envDev), path.join(dirTemp, files.envDev))
+}
+
+const modifyFiles = async () => {
+    console.log('🤟 \x1B[32m- Modifying files... \x1B[0m')
+    // .env
+    await fs.promises.unlink(path.join(files.envDev))
+    await fs.promises.copyFile(path.join(files.envProd), path.join(files.envDev))
+    await fs.promises.writeFile(path.join(files.envDev), env)
+}
+
+const restoreFiles = async () => {
+    console.log('🤟 \x1B[32m- Restore files... \x1B[0m')
+    await fs.promises.unlink(path.join(files.envDev))
+    await fs.promises.copyFile(path.join(dirTemp, files.envDev), path.join(files.envDev))
+    await fs.promises.rm(dirTemp, { recursive: true, force: true })
+}
+
+await createDirectories()
+await backupFiles()
+await modifyFiles()
+
+console.log('🤟 \x1B[32m- Creating Docker image... \x1B[0m')
+exec(
+    `docker build --build-arg SERVER_PORT=${process.env.SERVER_PORT} -t ${image} .`,
+    async (error, stdout, stderr) => {
+        if (error) {
+            console.log('🤬 \x1B[31m- docker build error: \x1B[0m', error)
+            await restoreFiles()
+            return
+        }
+        console.log(`👀 stdout: ${stdout}`)
+        console.error(`👀 stderr: ${stderr}`)
+
+        console.log('🤟 \x1B[32m- Running docker compose... \x1B[0m')
+        exec(`docker-compose -f docker-compose.prod.yml up -d --remove-orphans`, async (error, stdout, stderr) => {
+            if (error) {
+                console.log('🤬 \x1B[31m- docker compose error: \x1B[0m', error)
+                await restoreFiles()
+                return
+            }
+            console.log(`👀 stdout: ${stdout}`)
+            console.error(`👀 stderr: ${stderr}`)
+
+            await restoreFiles()
+        })
+    },
+)
---
+++ @wordpress/plugins/resume-item/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '6ca8fd5459e8508cdfdc');
