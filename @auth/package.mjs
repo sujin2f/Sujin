12fbe6757695292d59380a7ac7b7ffd65448699e
Sujin Choi <sujin.2f@gmail.com>
2025-11-26 03:06:11 -0500

    .

--- @auth/package.mjs
+++ @auth/package.mjs
@@ -15,8 +15,7 @@ const dirCommonModules = {
 
 const files = {
     tsConfig: 'tsconfig.json',
-    envDev: '.env',
-    envProd: '.env.production',
+    env: '.env',
     webpack: 'webpack.config.mjs',
 }
 
@@ -25,25 +24,17 @@ import packageJson from './package.json' with { type: 'json' }
 const VERSION = packageJson.version
 
 // Overwrite VERSION info
-let env = await fs.promises.readFile(path.join(files.envProd), 'utf-8')
-let envDev = await fs.promises.readFile(path.join(files.envDev), 'utf-8')
+let env = await fs.promises.readFile(path.join(files.env), 'utf-8')
 env = env.replace(/VERSION=[0-9.beta-]+\n/g, '')
 env += `VERSION=${VERSION}\n`
-envDev = env.replace(/VERSION=[0-9.beta-]+\n/g, '')
-envDev += `VERSION=${VERSION}\n`
-await fs.promises.writeFile(path.join(files.envProd), envDev)
-await fs.promises.writeFile(path.join(files.envDev), envDev)
+await fs.promises.writeFile(path.join(files.env), env)
 console.log('🤟 \x1B[32m- Version updated. \x1B[0m')
 
 // Check if Docker image exists
 const image = `sujin2f/auth:${VERSION}`
-const { stdout, stderr } = await execPromise(`docker image ls ${image}`)
+const { stdout } = await execPromise(`sudo docker image ls ${image}`)
 if (stdout.includes(image)) {
     console.error(`⛈️ Image ${image} already exists.`)
-    process.exit(1)
-}
-if (stderr) {
-    console.error(`👀 stderr: ${stderr}`)
     process.exit(1)
 }
 
@@ -80,9 +71,6 @@ const backupFiles = async () => {
     // tsconfig.webpack.json
     await fs.promises.copyFile(path.join(files.tsConfig), path.join(dirTemp, files.tsConfig))
 
-    // .env
-    await fs.promises.copyFile(path.join(files.envDev), path.join(dirTemp, files.envDev))
-
     // webpack
     await fs.promises.copyFile(path.join(files.webpack), path.join(dirTemp, files.webpack))
 }
@@ -92,11 +80,6 @@ const modifyFiles = async () => {
     // tsconfig.webpack.json
     await fs.promises.writeFile(path.join(files.tsConfig), JSON.stringify(tsConfig, null, 2))
 
-    // .env
-    await fs.promises.unlink(path.join(files.envDev))
-    await fs.promises.copyFile(path.join(files.envProd), path.join(files.envDev))
-    await fs.promises.writeFile(path.join(files.envDev), env)
-
     // webpack
     await fs.promises.writeFile(path.join(files.webpack), webpack)
 }
@@ -104,11 +87,9 @@ const modifyFiles = async () => {
 const restoreFiles = async () => {
     console.log('🤟 \x1B[32m- Restore files... \x1B[0m')
     await fs.promises.unlink(path.join(files.tsConfig))
-    await fs.promises.unlink(path.join(files.envDev))
     await fs.promises.unlink(path.join(files.webpack))
 
     await fs.promises.copyFile(path.join(dirTemp, files.tsConfig), path.join(files.tsConfig))
-    await fs.promises.copyFile(path.join(dirTemp, files.envDev), path.join(files.envDev))
     await fs.promises.copyFile(path.join(dirTemp, files.webpack), path.join(files.webpack))
 
     await fs.promises.rm(dirModule, { recursive: true, force: true })
@@ -120,28 +101,31 @@ await backupFiles()
 await modifyFiles()
 
 console.log(`🤟 \x1B[32m- Creating Docker image ${image} with port ${process.env.SERVER_PORT}... \x1B[0m`)
-exec(`docker build --build-arg SERVER_PORT=${process.env.SERVER_PORT} -t ${image} .`, async (error, stdout, stderr) => {
-    if (error) {
-        console.error('🤬 \x1B[31m- docker build error: \x1B[0m', error)
-        await restoreFiles()
-        return
-    }
-    console.log(`👀 stdout: ${stdout}`)
-    console.error(`👀 stderr: ${stderr}`)
-
-    // Delay 1 sec for finishing build
-    setTimeout(() => {}, 1000)
-
-    console.log('🤟 \x1B[32m- Running docker compose... \x1B[0m')
-    exec(`docker-compose up -d --remove-orphans`, async (error, stdout, stderr) => {
+exec(
+    `sudo docker build --build-arg SERVER_PORT=${process.env.SERVER_PORT} -t ${image} .`,
+    async (error, stdout, stderr) => {
         if (error) {
-            console.error('🤬 \x1B[31m- docker compose error: \x1B[0m', error)
+            console.error('🤬 \x1B[31m- docker build error: \x1B[0m', error)
             await restoreFiles()
             return
         }
         console.log(`👀 stdout: ${stdout}`)
         console.error(`👀 stderr: ${stderr}`)
 
-        await restoreFiles()
-    })
-})
+        // Delay 1 sec for finishing build
+        setTimeout(() => {}, 1000)
+
+        console.log('🤟 \x1B[32m- Running docker compose... \x1B[0m')
+        exec(`sudo docker-compose up -d --remove-orphans`, async (error, stdout, stderr) => {
+            if (error) {
+                console.error('🤬 \x1B[31m- docker compose error: \x1B[0m', error)
+                await restoreFiles()
+                return
+            }
+            console.log(`👀 stdout: ${stdout}`)
+            console.error(`👀 stderr: ${stderr}`)
+
+            await restoreFiles()
+        })
+    },
+)
