314f432bf64dbb1e3458b01cb8eefb96c0465bcf
Sujin Choi <sujin.2f@gmail.com>
2025-11-26 00:05:01 -0500

    production

--- @graphql/package.mjs
+++ @graphql/package.mjs
@@ -39,7 +39,7 @@ console.log('🤟 \x1B[32m- Version updated. \x1B[0m')
 
 // Check if Docker image exists
 const image = `sujin2f/graphql:${VERSION}`
-const { stdout, stderr } = await execPromise(`docker image ls ${image}`)
+const { stdout, stderr } = await execPromise(`sudo docker image ls ${image}`)
 if (stdout.includes(image)) {
     console.error(`⛈️ Image ${image} already exists.`)
     process.exit(1)
