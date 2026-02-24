0b9c793fc24e6445b1f6f474b7b8682964596d6f
Sujin <sujin.2f@gmail.com>
2026-02-24 16:53:37 -0500

    Add user a Focus info #219 (#222)

--- @graphql/src/resolvers/focus/createBookmark.ts
+++ @graphql/src/resolvers/focus/createBookmark.ts
@@ -10,16 +10,20 @@ import { COLLECTION } from '@sujin/lib/constants'
 /* Utils */
 import { verifyAccessToken } from '@src/utils/security'
 import { removeCache } from '@src/utils/redis/cache'
+import { createFocusBrowser } from '@src/utils/mongo/focus'
 
 export const createBookmark = async (message: T_Focus_Message, token: string): Promise<string> => {
     Logger.info('🤞 createFocusMessage start')
     const user = await verifyAccessToken(token)
     if (!user._id) throw new Error()
+    const userId = new Types.ObjectId(user._id)
 
-    const exists = await Focus.findOne({ type: 'bookmark', key: message.key })
+    await createFocusBrowser(userId, message)
+
+    const exists = await Focus.findOne({ type: message.type, key: message.key, user: userId })
     if (!exists) {
         const expires = Math.round(new Date().getTime() / HOUR_IN_MS) + 24 // Expires in 24 hours
-        await Focus.insertOne({ ...message, user: new Types.ObjectId(user._id), expires })
+        await Focus.insertOne({ ...message, user: userId, expires })
         await removeCache(`${COLLECTION.BOOKMARK}-${user.email}`)
         Logger.info('🤞 createFocusMessage created')
         return 'ok'
